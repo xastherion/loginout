c593dff270315f7e50563ac4abdacd75f62a09c3
oxoloxo <carlos.gonzalez@pcanda.de>
2017-09-24 14:39:41 +0200

    Mit Tab als trenner und csv als filename

--- abmeldung.py
+++ abmeldung.py
@@ -3,13 +3,13 @@ import time
 
 _Logoutzeit_=time.ctime()
 _AbsoluteZeitOut_=str(time.time())
-_Tabs_="\t\t"
+_Tabs_="\t"
 
-_Protokoll_linie_=(_Tabs_+"Out : "+_Logoutzeit_+_Tabs_+_AbsoluteZeitOut_+"\n")
+_Protokoll_linie_=(_Tabs_+_Logoutzeit_+_Tabs_+_AbsoluteZeitOut_+"\n")
 # print(_AbsoluteZeitIn_ - _AbsoluteZeitOut_)
 
 
-with open('protokoll_loginout.dat',"a+") as _Protokoll_:
+with open('protokoll_loginout.csv',"a+") as _Protokoll_:
     _Protokoll_.write(_Protokoll_linie_)
 _Protokoll_.closed
 
--- anmeldung.py
+++ anmeldung.py
@@ -1,16 +1,14 @@
 import os
 import time
-global _AbsoluteZeitIn_
 
 _Rechnername_ = os.uname()[1]
 _Benutzername_ = os.getlogin()
 _Loginzeit_ = time.ctime()
 _AbsoluteZeitIn_ = str(time.time())
-print(_AbsoluteZeitIn_)
-_Tabs_ = "\t\t"
+_Tabs_ = "\t"
 
-_Protokoll_linie_ = ( _Rechnername_ + _Tabs_ + _Benutzername_ + _Tabs_ + " In : " + _Loginzeit_ + _Tabs_ +_AbsoluteZeitIn_ + " --> ")
+_Protokoll_linie_ = ( _Rechnername_ + _Tabs_ + _Benutzername_ + _Tabs_ + _Loginzeit_ + _Tabs_ +_AbsoluteZeitIn_)
 
-with open('protokoll_loginout.dat','a') as _Protokoll_:
+with open('protokoll_loginout.csv','a') as _Protokoll_:
     _Protokoll_.write(_Protokoll_linie_)
 _Protokoll_.closed
